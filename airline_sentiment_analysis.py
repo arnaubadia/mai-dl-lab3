825bbd7db57265bd86ab2ec74536bf2af8666e34
arnaubadia <arnaubadia96@gmail.com>
2019-04-21 21:14:30 +0200

    some changes

--- airline_sentiment_analysis.py
+++ airline_sentiment_analysis.py
@@ -1,12 +1,19 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import re
+import emoji
 from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 
 
 DATASET_PATH = 'Tweets.csv'
 embedding_dim = 100
+
+
+"""
+********************** PREPROCESSING AND EMBEDDINGS ***************************
+"""
 
 def preprocess_tweet(t):
     # remove mention of the airline twitter account
@@ -18,6 +25,13 @@ def preprocess_tweet(t):
     # in informal context such as twitter it is common to remove
     t = re.sub(r"[?]+", '?', t)
     t = re.sub(r"[!]+", '!', t)
+    # separate emojis
+    t2 = []
+    for i in range(len(t)):
+         t2.append(t[i])
+         if i < len(t)-1 and t[i] != ' ' and t[i+1] in emoji.UNICODE_EMOJI:
+             t2.append(' ')
+    t = ''.join(t2)
     return t
 
 import string
@@ -49,6 +63,7 @@ labels = pd.get_dummies(df['airline_sentiment']).values
 
 # random train-val-test partition (80-10-10)
 indices = list(range(len(x)))
+np.random.seed(42)
 np.random.shuffle(indices)
 train_indices = indices[:int(0.8*len(x))]
 val_indices = indices[int(0.8*len(x)):int(0.9*len(x))]
@@ -56,10 +71,13 @@ test_indices = indices[int(0.9*len(x)):]
 
 # max length (word_length) could be more in a real unseen tweet, but we know
 # that it is limited by 140 characters.
+
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(x[train_indices])
+# tokenizer.fit_on_texts(x)
 word_index = tokenizer.word_index
 max_length = max([len(s.split()) for s in x[train_indices]])
+# max_length = max([len(s.split()) for s in x])
 vocabulary_size = len(tokenizer.word_index) + 1
 x = tokenizer.texts_to_sequences(x)
 x = pad_sequences(x, maxlen=max_length, padding='post')
@@ -73,8 +91,9 @@ y_test = labels[test_indices]
 
 # read pretrained embeddings into dict
 train_embedding(tweets)
-# embedding_path = 'airline_tweets_embedding.txt'
-embedding_path = 'glove.twitter.27B/glove.twitter.27B.100d.txt'
+embedding_path = 'airline_tweets_embedding.txt'
+# embedding_path = 'glove.twitter.27B/glove.twitter.27B.100d.txt'
+# embedding_path = 'glove.42B.300d.txt'
 embeddings_index = {}
 f = open(embedding_path, encoding='utf-8')
 for line in f:
@@ -94,6 +113,10 @@ for word, index in word_index.items():
         embedding_matrix[index] = embedding_vector
 
 
+"""
+********************** NEURAL NETWORK **************************************
+"""
+
 print('x train shape: ', x_train.shape)
 print('y train shape: ', y_train.shape)
 print('x val shape: ', x_val.shape)
@@ -107,18 +130,104 @@ nn = Sequential()
 nn.add(Embedding(vocabulary_size, embedding_dim, embeddings_initializer=Constant(embedding_matrix),
                 input_length=max_length, trainable=False))
 #nn.add(GRU(units=32, dropout=0.1, recurrent_dropout=0.1))
-#nn.add(GRU(units=32, dropout=0.2, recurrent_dropout=0.2))
-nn.add(Conv1D(256, 10, activation='relu'))
-nn.add(Conv1D(128, 10, activation='relu'))
-nn.add(Flatten())
-nn.add(Dense(128, activation='relu'))
+nn.add(GRU(units=64, dropout=0.2, recurrent_dropout=0.2))
+#nn.add(Conv1D(256, 10, activation='relu'))
+#nn.add(Conv1D(128, 10, activation='relu'))
+#nn.add(Flatten())
+#nn.add(Dense(128, activation='relu'))
 nn.add(Dense(3, activation='softmax'))
 
 nn.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
+mcp_save = ModelCheckpoint('best-weights.hdf5', save_best_only=True, monitor='val_loss', mode='min')
 
 
-nn.fit(x_train, y_train, batch_size=128, epochs=25, validation_data=(x_val, y_val))
+history = nn.fit(x_train, y_train, batch_size=128, epochs=100, validation_data=(x_val, y_val),
+                callbacks=[earlyStopping, mcp_save])
+
+"""
+********************** PLOTS AND RESULTS **************************************
+"""
+#Restore the model with the best weights we found during training
+nn.load_weights('best-weights.hdf5')
+
+#Accuracy plot
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train','val'], loc='upper left')
+plt.savefig('accuracy.pdf')
+plt.close()
+#Loss plot
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train','val'], loc='upper left')
+plt.savefig('loss.pdf')
+
+
+# from https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    #classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
 
 #Confusion Matrix
 from sklearn.metrics import classification_report,confusion_matrix
@@ -131,5 +240,10 @@ y_pred = np.argmax(Y_pred, axis=1)
 #Plot statistics
 print( 'Analysis of results' )
 target_names = ['0','1','2']
+print('Accuracy: ', sum(np.argmax(y_test,axis=1) == y_pred)/len(y_pred))
 print(classification_report(np.argmax(y_test,axis=1), y_pred,target_names=target_names))
 print(confusion_matrix(np.argmax(y_test,axis=1), y_pred))
+
+plot_confusion_matrix(np.argmax(y_test,axis=1), y_pred, target_names, normalize=True)
+plt.savefig('confusion_matrix.pdf')
+plt.close()
--- airline_sentiment_analysis_naive_bayes.py
+++ airline_sentiment_analysis_naive_bayes.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import re
+import emoji
+import matplotlib.pyplot as plt
 from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 
@@ -18,6 +20,13 @@ def preprocess_tweet(t):
     # in informal context such as twitter it is common to remove
     t = re.sub(r"[?]+", '?', t)
     t = re.sub(r"[!]+", '!', t)
+    # separate emojis
+    t2 = []
+    for i in range(len(t)):
+         t2.append(t[i])
+         if i < len(t)-1 and t[i] != ' ' and t[i+1] in emoji.UNICODE_EMOJI:
+             t2.append(' ')
+    t = ''.join(t2)
     return t
 
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
@@ -29,6 +38,7 @@ labels = df['airline_sentiment'].values
 
 # random train-val-test partition (80-10-10)
 indices = list(range(len(x)))
+np.random.seed(42)
 np.random.shuffle(indices)
 train_indices = indices[:int(0.8*len(x))]
 val_indices = indices[int(0.8*len(x)):int(0.9*len(x))]
@@ -50,10 +60,70 @@ from sklearn.naive_bayes import MultinomialNB
 nbc = MultinomialNB()
 nbc.fit(X_train_one_hot, y_train)
 
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    #classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
+
 from sklearn.metrics import classification_report,confusion_matrix
 y_pred = nbc.predict(X_test_one_hot)
 score = nbc.score(X_test_one_hot, y_test)
-print(score)
+print('Accuracy: ', score)
 
+target_names = ['negative', 'neutral', 'positive']
+plot_confusion_matrix(y_test, y_pred, target_names, normalize=True)
+plt.show()
 print(classification_report(y_test,y_pred))
 print(confusion_matrix(y_test,y_pred))
